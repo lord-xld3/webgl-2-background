3e8502c57e09abf31f5569c0ef36cce2792f6f81
Kyle Simmons <simmonskyleb@gmail.com>
2023-11-23 13:02:17 -0600

    mapped out uniforms

--- src/script.js
+++ src/script.js
@@ -1,81 +1,108 @@
 // script.js
-// Generates uniform information for all shaders in the shaders/ directory
-// Outputs the information to json/{shaderName}.json
-
 const fs = require('fs');
 
-function uniformCrawler(shaderDir) {
-    fs.readdirSync(shaderDir).forEach((file) => {
+// Input path for shaders
+const shadersDirectory = 'shaders/';
+
+// Output uniform info to outputPath/{shaderName}.json
+const outputDirectory = 'json/';
+
+function crawlUniforms() {
+    fs.readdirSync(shadersDirectory).forEach((file) => {
         if (file.endsWith('.glsl')) {
-            const filename = file.split('.')[0];
-            const shader = fs.readFileSync(`${shaderDir}/${file}`, 'utf8');
-            const uniforms = shader.match(/uniform\s+\w+\s+\w+\s*;/g);
-            let shaderData = {shader: filename, uniforms: []}
-            
+            const shaderName = file.split('.')[0];
+            const shaderCode = fs.readFileSync(`${shadersDirectory}/${file}`, 'utf8');
+            const uniforms = shaderCode.match(/uniform\s+\w+\s+\w+\s*;/g);
+            let shaderData = { shader: shaderName, uniforms: [] };
+
             if (uniforms) {
                 uniforms.forEach((uniform) => {
-                    const [uniformName, basicType, uniformElements] = getUniformInfo(uniform);
-                    let data = {
+                    const [uniformName, primative, type, uniformElements, uniformFunction] = extractUniformInfo(uniform);
+                    shaderData.uniforms.push({
                         name: uniformName,
-                        type: basicType,
+                        primative: primative,
+                        type,
                         elements: uniformElements,
-                    };
-                    shaderData.uniforms.push(data);
+                        function: uniformFunction,
+                    });
                 });
-                // Write uniform info to json/{shaderName}.json
-                fs.writeFileSync(`json/${filename}.json`, JSON.stringify(shaderData, null, 4));
+                mapDefaultValues(shaderData);
             }
+            // Else, don't output a file without uniforms
         }
     });
-
-    
 }
 
 // Extract uniform info
-function getUniformInfo(uniform) {
+function extractUniformInfo(uniform) {
     const regex = /uniform\s+([\w\s]+)\s+(\w+)\s*;/;
     const matches = uniform.match(regex);
 
     if (matches && matches.length >= 3) {
         const uniformName = matches[2].trim();
-        const uniformType = matches[1].trim();
-        const [basicType, uniformElements] = getTypeAndElements(uniformType);
+        const type = matches[1].trim();
+        const [primative, uniformElements, uniformFunction] = mapTypes(type);
 
-        return [uniformName, basicType, uniformElements];
+        return [uniformName, primative, type, uniformElements, uniformFunction];
     }
 
-    return ['', '', ''];
+    return ['', '', '', '', ''];
 }
 
-// Extracts the type and number of elements from a uniform
-function getTypeAndElements(uniformType) {
+// Map uniform type to primative type, elements, and uniform function
+function mapTypes(type) {
     const typeWithElements = {
-        vec2: ['float', 2],
-        vec3: ['float', 3],
-        vec4: ['float', 4],
-
-        ivec2: ['int', 2],
-        ivec3: ['int', 3],
-        ivec4: ['int', 4],
-
-        uvec2: ['uint', 2],
-        uvec3: ['uint', 3],
-        uvec4: ['uint', 4],
-
-        bvec2: ['bool', 2],
-        bvec3: ['bool', 3],
-        bvec4: ['bool', 4],
-
-        mat2: ['float', 4],
-        mat3: ['float', 9],
-        mat4: ['float', 16],
-
-        float: ['float', 1],
-        int: ['int', 1],
-        uint: ['uint', 1],
-        bool: ['bool', 1],
+        vec2: ['float', 2, 'uniform2fv'],
+        vec3: ['float', 3, 'uniform3fv'],
+        vec4: ['float', 4, 'uniform4fv'],
+        ivec2: ['int', 2, 'uniform2iv'],
+        ivec3: ['int', 3, 'uniform3iv'],
+        ivec4: ['int', 4, 'uniform4iv'],
+        uvec2: ['uint', 2, 'uniform2uv'],
+        uvec3: ['uint', 3, 'uniform3uv'],
+        uvec4: ['uint', 4, 'uniform4uv'],
+        bvec2: ['bool', 2, 'uniform2bv'],
+        bvec3: ['bool', 3, 'uniform3bv'],
+        bvec4: ['bool', 4, 'uniform4bv'],
+        mat2: ['float', 4, 'uniformMatrix2fv'],
+        mat3: ['float', 9, 'uniformMatrix3fv'],
+        mat4: ['float', 16, 'uniformMatrix4fv'],
+        float: ['float', 1, 'uniform1f'],
+        int: ['int', 1, 'uniform1i'],
+        uint: ['uint', 1, 'uniform1u'],
+        bool: ['bool', 1, 'uniform1b'],
     };
-    return typeWithElements[uniformType];
+    return typeWithElements[type];
 }
 
-uniformCrawler('shaders/');
+// Map default values to uniforms for controls
+function mapDefaultValues(data) {
+    let defaults = { shader: data.shader, uniforms: [] };
+    data.uniforms.forEach((uniform) => {
+        let defaultUniform = {
+            name: uniform.name,
+            function: uniform.function,
+            elements: uniform.elements,
+        };
+        switch (uniform.primative) {
+            case 'float':
+                Object.assign(defaultUniform, { min: 0, max: 1, step: 0.1, init: 0.5 });
+                break;
+            case 'int':
+                Object.assign(defaultUniform, { min: -10, max: 10, step: 1, init: 0 });
+                break;
+            case 'uint':
+                Object.assign(defaultUniform, { min: 0, max: 10, step: 1, init: 5 });
+                break;
+            case 'bool':
+                Object.assign(defaultUniform, { init: false });
+                break;
+            default:
+                throw new Error(`Unrecognized uniform type ${uniform.primative}`);
+        }
+        defaults.uniforms.push(defaultUniform);
+    });
+    fs.writeFileSync(`${outputDirectory}${data.shader}.json`, JSON.stringify(defaults, null, 4));
+}
+
+crawlUniforms();
