a188a0cc9329181f737b1b32a760cfbcbd37bfdc
Kyle Simmons <simmonskyleb@gmail.com>
2023-12-21 11:41:57 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ void main() {
 }`;
 
 const fsShader = `#version 300 es
-precision mediump float;
+precision highp float;
 in vec3 v_color;
 in vec2 v_texcoord;
 
@@ -114,6 +114,8 @@ image.onload = function() {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 }
 
 gl.enable(gl.DEPTH_TEST);
