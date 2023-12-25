b75f7a366dab41f95731d64e5d8df27126fe7ba9
lord-xld3 <simmons.kyle.b@gmail.com>
2023-12-25 04:00:01 -0600

    normal mapping

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ console.log(`Vendor: ${gl.getParameter(gl.VENDOR)}`);
 const vsShader = `#version 300 es
 in vec4 a_position;
 in vec3 a_color;
-in vec3 a_normal;
 in vec2 a_texcoord;
 
 uniform mat4 u_modelMatrix;
@@ -24,26 +23,18 @@ uniform mat4 u_viewMatrix;
 uniform mat4 u_projectionMatrix;
 uniform vec3 u_lightWorldPosition;
 uniform vec3 u_viewWorldPosition;
-uniform vec3 u_diffuseColor;
 
 out vec3 v_color;
 out vec2 v_texcoord;
-out vec3 v_normal;
 out vec3 v_surfaceToLight;
 out vec3 v_surfaceToView;
 
 void main() {
 	vec4 modelViewPosition = u_viewMatrix * u_modelMatrix * a_position;
-	
 	v_surfaceToLight = normalize(u_lightWorldPosition - modelViewPosition.xyz);
 	v_surfaceToView = normalize(u_viewWorldPosition - modelViewPosition.xyz);
-
-	// Diffuse lighting
-	vec3 normal = normalize(mat3(u_modelMatrix) * a_normal);
-	v_color = a_color.rgb * max(dot(normal, v_surfaceToLight), 0.0);
-
 	v_texcoord = a_texcoord;
-	v_normal = normal;
+	v_color = a_color;
 	gl_Position = u_projectionMatrix * modelViewPosition;
 }`;
 
@@ -51,11 +42,11 @@ const fsShader = `#version 300 es
 precision highp float;
 in vec3 v_color;
 in vec2 v_texcoord;
-in vec3 v_normal;
 in vec3 v_surfaceToLight;
 in vec3 v_surfaceToView;
 
 uniform sampler2D u_texture;
+uniform sampler2D u_normalMap;
 uniform float u_shininess;
 uniform vec3 u_specularColor;
 uniform vec3 u_ambientLight;
@@ -63,13 +54,22 @@ uniform vec3 u_ambientLight;
 out vec4 outColor;
 
 void main() {
+	// Normal mapping
+	vec3 normal = texture(u_normalMap, v_texcoord).xyz * 2.0 - 1.0;
+	normal = normalize(normal);
+
+	// Diffuse lighting
+	float light = dot(normal, v_surfaceToLight);
+	vec3 diffuse = u_ambientLight + (v_color * light);
+
 	// Specular lighting
 	vec3 halfVector = normalize(v_surfaceToLight + v_surfaceToView);
-	float dotFromDirection = dot(v_normal, halfVector);
+	float dotFromDirection = dot(normal, halfVector);
 	float specularFactor = pow(dotFromDirection, u_shininess);
 	vec3 specular = u_specularColor * specularFactor;
+
 	vec4 texelColor = texture(u_texture, v_texcoord);
-	outColor = vec4(texelColor.rgb * (v_color + u_ambientLight) + specular, texelColor.a);
+	outColor = vec4(diffuse * texelColor.rgb + specular, texelColor.a);
 }`;
 
 const shaderProgram = glUtils.makeProgram(gl, vsShader, fsShader);
@@ -78,7 +78,6 @@ const attribs = glUtils.getAttribLocations(gl, shaderProgram,
 	[
 		'a_position', 
 		'a_color',
-		'a_normal',
 		'a_texcoord',
 	]
 );
@@ -89,6 +88,7 @@ const uniforms = glUtils.getUniformLocations(gl, shaderProgram,
 		'u_viewMatrix',
 		'u_projectionMatrix',
 		'u_texture',
+		'u_normalMap',
 		'u_lightWorldPosition',
 		'u_viewWorldPosition',
 		'u_shininess',
@@ -97,56 +97,56 @@ const uniforms = glUtils.getUniformLocations(gl, shaderProgram,
 	]
 );
 
-let bufferStride = 11 * Float32Array.BYTES_PER_ELEMENT;
+let bufferStride = 8 * Float32Array.BYTES_PER_ELEMENT;
 glUtils.makeBuffer(gl,
 	new Float32Array([
 		// Front face
-		-1,-1, 1, 0.5, 0.5, 0.5, 0, 1, 0, 0, 1,
-		 1,-1, 1, 0.5, 0.5, 0.5, 1, 1, 0, 0, 1,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 0, 0, 1,
-		-1,-1, 1, 0.5, 0.5, 0.5, 0, 1, 0, 0, 1,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 0, 0, 1,
-		-1, 1, 1, 0.5, 0.5, 0.5, 0, 0, 0, 0, 1,
+		-1,-1, 1, 0.5, 0.5, 0.5, 0, 1,
+		 1,-1, 1, 0.5, 0.5, 0.5, 1, 1,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1, 1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1, 1, 1, 0.5, 0.5, 0.5, 0, 0,
 
 		// Back face
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 0, 0, -1,
-		 1,-1,-1, 0.5, 0.5, 0.5, 0, 0, 0, 0, -1,
-		 1, 1,-1, 0.5, 0.5, 0.5, 1, 0, 0, 0, -1,
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 0, 0, -1,
-		 1, 1,-1, 0.5, 0.5, 0.5, 1, 0, 0, 0, -1,
-		-1, 1,-1, 0.5, 0.5, 0.5, 1, 1, 0, 0, -1,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1,-1,-1, 0.5, 0.5, 0.5, 0, 0,
+		 1, 1,-1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1,-1, 0.5, 0.5, 0.5, 1, 0,
+		-1, 1,-1, 0.5, 0.5, 0.5, 1, 1,
 
 		// Top face
-		-1, 1,-1, 0.5, 0.5, 0.5, 0, 1, 0, 1, 0,
-		 1, 1,-1, 0.5, 0.5, 0.5, 0, 0, 0, 1, 0,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 0, 1, 0,
-		-1, 1,-1, 0.5, 0.5, 0.5, 0, 1, 0, 1, 0,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 0, 1, 0,
-		-1, 1, 1, 0.5, 0.5, 0.5, 1, 1, 0, 1, 0,
+		-1, 1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1,-1, 0.5, 0.5, 0.5, 0, 0,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1, 1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1, 1, 1, 0.5, 0.5, 0.5, 1, 1,
 
 		// Bottom face
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 0, -1, 0,
-		 1,-1,-1, 0.5, 0.5, 0.5, 1, 1, 0, -1, 0,
-		 1,-1, 1, 0.5, 0.5, 0.5, 1, 0, 0, -1, 0,
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 0, -1, 0,
-		 1,-1, 1, 0.5, 0.5, 0.5, 1, 0, 0, -1, 0,
-		-1,-1, 1, 0.5, 0.5, 0.5, 0, 0, 0, -1, 0,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1,-1,-1, 0.5, 0.5, 0.5, 1, 1,
+		 1,-1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1,-1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1, 1, 0.5, 0.5, 0.5, 0, 0,
 
 		// Right face
-		 1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 1, 0, 0,
-		 1, 1,-1, 0.5, 0.5, 0.5, 1, 1, 1, 0, 0,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 1, 0, 0,
-		 1,-1,-1, 0.5, 0.5, 0.5, 0, 1, 1, 0, 0,
-		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0, 1, 0, 0,
-		 1,-1, 1, 0.5, 0.5, 0.5, 0, 0, 1, 0, 0,
+		 1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1,-1, 0.5, 0.5, 0.5, 1, 1,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		 1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		 1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		 1,-1, 1, 0.5, 0.5, 0.5, 0, 0,
 
 		// Left face
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, -1, 0, 0,
-		-1, 1,-1, 0.5, 0.5, 0.5, 0, 0, -1, 0, 0,
-		-1, 1, 1, 0.5, 0.5, 0.5, 1, 0, -1, 0, 0,
-		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1, -1, 0, 0,
-		-1, 1, 1, 0.5, 0.5, 0.5, 1, 0, -1, 0, 0,
-		-1,-1, 1, 0.5, 0.5, 0.5, 0, 0, -1, 0, 0,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		-1, 1,-1, 0.5, 0.5, 0.5, 0, 0,
+		-1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1,-1, 0.5, 0.5, 0.5, 0, 1,
+		-1, 1, 1, 0.5, 0.5, 0.5, 1, 0,
+		-1,-1, 1, 0.5, 0.5, 0.5, 0, 0,
 	]),
 	gl.ARRAY_BUFFER,
 	gl.STATIC_DRAW
@@ -185,18 +185,8 @@ glUtils.setAttribPointer(gl,
 	}
 );
 
-glUtils.setAttribPointer(gl,
-	attribs.a_normal,
-	{
-		size: 3,
-		type: gl.FLOAT,
-		normalize: false,
-		stride: bufferStride,
-		offset: 8 * Float32Array.BYTES_PER_ELEMENT,
-	}
-);
-
 const texture = gl.createTexture();
+gl.activeTexture(gl.TEXTURE0);
 gl.bindTexture(gl.TEXTURE_2D, texture);
 
 var image = new Image();
@@ -204,6 +194,19 @@ image.src = 'img/cobble.png';
 image.onload = function() {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
+
+const normalMap = gl.createTexture();
+gl.activeTexture(gl.TEXTURE1);
+gl.bindTexture(gl.TEXTURE_2D, normalMap);
+var normalImage = new Image();
+normalImage.src = 'img/cobble_normal.png';
+normalImage.onload = function() {
+	gl.bindTexture(gl.TEXTURE_2D, normalMap);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, normalImage);
 	gl.generateMipmap(gl.TEXTURE_2D);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -240,6 +243,8 @@ gl.uniform3fv(uniforms.u_viewWorldPosition, cameraPosition);
 gl.uniform1f(uniforms.u_shininess, 256);
 gl.uniform3fv(uniforms.u_specularColor, [1, 1, 1]);
 gl.uniform3fv(uniforms.u_ambientLight, [0.1, 0.1, 0.1]);
+gl.uniform1i(uniforms.u_texture, 0);
+gl.uniform1i(uniforms.u_normalMap, 1);
 
 window.dispatchEvent(new Event('resize'));
 render();
